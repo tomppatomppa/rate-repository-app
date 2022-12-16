a7c70be67076d847f530f0102f53956ebd7b8325
tomi <tomiwest@hotmail.com>
2022-12-16 11:05:45 +0200

    Exercise 10.3: the reviewed repositories list

---
+++ src/components/Main.jsx
@@ -0,0 +1,28 @@
+import Constants from 'expo-constants';
+import { StyleSheet, View } from 'react-native';
+import RepositoryList from './RepositoryList';
+import Text from './Text';
+
+const styles = StyleSheet.create({
+  container: {
+    marginTop: Constants.statusBarHeight,
+    flexGrow: 1,
+    flexShrink: 1,
+  },
+});
+
+const Main = () => {
+  return (
+    <View style={styles.container}>
+      <RepositoryList />
+      <Text>Simple text</Text>
+      <Text style={{ paddingBottom: 10 }}>Text with custom style</Text>
+      <Text fontWeight="bold" fontSize="subheading">
+        Bold subheading
+      </Text>
+      <Text color="textSecondary">Text with secondary color</Text>
+    </View>
+  );
+};
+
+export default Main;
